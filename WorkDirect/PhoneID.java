2cf82590155a66a50b5bbeb24b3f17e76cb990e9
EnverGeekBrains <41239252+EnverGeekBrains@users.noreply.github.com>
2018-09-23 19:54:27 +0300

    Update PhoneID.java

--- WorkDirect/PhoneID.java
+++ WorkDirect/PhoneID.java
@@ -3,7 +3,7 @@ package ru.eskendarov.ea.homework;
 import java.util.*;
 
 public class PhoneID {
-    private Map<String, Set<String>> values = new TreeMap<>();
+    private final Map<String, Set<String>> values = new TreeMap<>();
 
     public void add(String name, String number) {
         if (values.containsKey(name)) {
